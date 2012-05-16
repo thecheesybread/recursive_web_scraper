9be3a4870fd1f6e51a2947b1c21be47b7c474a02
Lu Cheng <lu.cheng@berkeley.edu>
2012-05-15 22:00:05 -0700

    working recursive html downloader

--- get_html.py
+++ get_html.py
@@ -2,19 +2,29 @@ import urllib2
 from pymongo import Connection, ASCENDING, DESCENDING
 import re
 from Queue import Queue
+import os
+import sys
+import time
+import random
 
-random_wait = True
-wait_time = 3
 
-"""Determines which urls to scrape through storing previous entries in database"""
-url_to_scrape = sys.argv[1]
-connection = Connection('localhost', 27017)
-db = connection.get_html
-url_collection = db[url_to_scrape]
 
-cookie_handler = urllib2.HTTPCookieProcessor()
-url_opener = urllib2.build_opener(MyHTTPRedirectHandler, cookie_handler)
-url_opener.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5')]
+def set_up_globals():
+    global random_wait, wait_time, url_to_scrape, connection, db, url_collection, cookie_handler, url_opener, url_short
+    
+    random_wait = True
+    wait_time = 2
+
+    """Determines which urls to scrape through storing previous entries in database"""
+    url_to_scrape = sys.argv[1]
+    url_short = url_to_scrape.replace('http://', '')
+    connection = Connection('localhost', 27017)
+    db = connection.get_html
+    url_collection = db[url_to_scrape]
+    
+    cookie_handler = urllib2.HTTPCookieProcessor()
+    url_opener = urllib2.build_opener(MyHTTPRedirectHandler, cookie_handler)
+    url_opener.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5')]
 
 def scrape_html():
     all_entries = url_collection.find({'counter' : {'$exists' : True}}).sort('counter', DESCENDING)
@@ -42,8 +52,21 @@ def scrape_html():
     
     begin_get(url_queue, explored_set, current_counter)
 
-    
-    
+
+directories_exist = set()
+directory_regex = re.compile('(.*)/[^/]+')
+url_regex = re.compile('(.*)/')
+def ensure_path(url):
+    if '/' not in url:
+        return
+    path = directory_regex.search(url).group(1)
+    if path in directories_exist:
+        return
+    if not os.path.exists(path):
+        os.makedirs(path)
+        directories_exist.add(path)
+
+
 def begin_get(url_queue, explored_set, current_counter):
     save_counter = 0
     while not url_queue.empty():
@@ -52,15 +75,31 @@ def begin_get(url_queue, explored_set, current_counter):
             current_counter = save_entry(explored_set, url_queue, current_counter)
             save_counter = 0
         url = url_queue.get()
+        print 'Working on ' + url
         page = url_opener.open(url)
         html = page.read()
-        url_path = url.split('/')
-
+        if not url_short in url:
+            continue
+        url = url.replace('http://', '')
+        account_for_last_slash = url_regex.search(url)
+        if account_for_last_slash and account_for_last_slash.group(0) == url:
+            url = account_for_last_slash.group(1)
+        ensure_path(url)
+        f = open(url + '_file', 'w')
+        f.write(html)
+        f.close()
+        random_wait = random.randint(0, 100)/30.0
+        time.sleep(wait_time + random_wait)
+        
         """Finds new urls"""
-        links = [x.group(1) for x in re.finditer(r'href="([^"]*)"', html)
+        links = [x.group(1) for x in re.finditer(r'href="([^"]*)"', html)]
+        for x in links:
+            if url_to_scrape in x and x not in explored_set:
+                explored_set.add(x)
+                url_queue.put(x)
         
 
-    print 'finish'
+    print 'finished'
     
 
     
@@ -106,5 +145,6 @@ class MyHTTPRedirectHandler(urllib2.HTTPRedirectHandler):
 
 
 
-def __name__ == '__main__':
+if __name__ == '__main__':
+    set_up_globals()
     scrape_html()
